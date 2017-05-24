984b7d82e497beedef4985d44b40549bfbbcd2df
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2017-05-24 09:27:58 +0000

    gen.py: generate the Docker Terraform plan.

---
+++ gen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import os
+import jinja2
+
+def render(tpl_path, context):
+    path, filename = os.path.split(tpl_path)
+    return jinja2.Environment(
+        loader=jinja2.FileSystemLoader(path or './')
+    ).get_template(filename).render(context)
+
+context = {
+  'hostnames': {
+    'manager1': {},
+    'worker1': {},
+    'worker2': {},
+  }
+}
+
+with open('docker.tf', 'w') as h:
+  result = render('docker.tmpl', context)
+  h.write(result)
